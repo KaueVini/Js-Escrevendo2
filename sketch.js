da8ebc85818a3b1a9bba09981ca39a53f7b86fc4
KaueVini <132962559+KaueVini@users.noreply.github.com>
2023-07-11 15:26:35 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  background(0);
+}
+
+function draw() {
+  stroke("blue");
+  fill("red");
+  
+  if(mouseIsPressed)[
+    circle(mouseX, mouseY, 20)
+  ]
+}
